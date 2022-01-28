fb42c7286d71520df7be12fe1f09209859768a8d
pedromartosjimenez <pedro.martos@sdelsol.com>
2022-01-28 09:21:06 +0000

    a

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import { HelloWorld} from './HelloWorld';
+import { useQuery, gql } from '@apollo/client'
+import { HelloWorld } from './HelloWorld';
+
+const QUERY = gql`
+  query getMyUsers {
+    users {
+      name
+      expenses {
+        amount
+      }
+    }
+  }
+`;
 
 function App() {
+  const res = useQuery(QUERY);
+  console.log(res.data)
+
   return (
     <div>
-      <HelloWorld/>
+      <HelloWorld />
+      {res.data && (
+        <ul>
+          {res.data.users.map(user => (
+            <li>
+              {user.name}
+              ({user.expenses.length} expenses)
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
 export default App;
+
--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvier } from '@apollo/client';
-import './index.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const ApolloClient = {
-
-};
+const client = new ApolloClient({
+  uri: 'https://8080-pedromartosjime-ctatrain-o2jgdryujch.ws-eu29.gitpod.io/v1/graphql',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
